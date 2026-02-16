e71e0f8ca3384cba49fd681b107e53d3dad8c2a0
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-16 19:48:32 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GnidenkoPA.Sprint6.Task4.V9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using Tyuiu.GnidenkoPA.Sprint6.Task4.V9;
+
+namespace Tuiu.GnidenkoPA.Sprint6.Task4.V9
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
