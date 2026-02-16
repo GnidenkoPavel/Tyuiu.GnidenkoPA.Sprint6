71f19112ad0a1c7a047df3e3f12f0d8d95f4aac3
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-16 23:49:25 +0500

    Task5

---
+++ Tyuiu.GnidenkoPA.Sprint6.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.GnidenkoPA.Sprint6.Task5.V8.Lib
+{
+    public class DataService : ISprint6Task5V8
+    {
+        public int len = 0;
+
+        public double[] LoadFromDataFile(string path)
+        {
+            
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    
+                    string[] numbers = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    len += numbers.Length;
+                }
+            }
+
+            
+            double[] allNumbers = new double[len];
+            int index = 0;
+
+          
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] numbers = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string num in numbers)
+                    {
+                        allNumbers[index] = Convert.ToDouble(num);
+                        index++;
+                    }
+                }
+            }
+
+            
+            double[] result = Array.FindAll(allNumbers, x => x < 0);
+
+          
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Math.Round(result[i], 3);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint6.Task5.V8.Test/DataServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Tyuiu.GnidenkoPA.Sprint6.Task5.V8.Lib;
+
+namespace Tyuiu.GnidenkoPA.Sprint6.Task5.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+           
+            string path = Path.GetTempFileName();
+
+          
+            File.WriteAllText(path, "-5 3 -7 8 -2 10 -4");
+
+      
+            DataService ds = new DataService();
+
+         
+            double[] result = ds.LoadFromDataFile(path);
+
+       
+            double[] expected = { -5, -7, -2, -4 };
+
+            
+            CollectionAssert.AreEqual(expected, result);
+
+           
+            File.Delete(path);
+        }
+
+        [TestMethod]
+        public void CheckFileExists()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InputFileTask5V8.txt";
+
+            
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            
+            Assert.IsTrue(fileExists, $"Файл {path} не найден!");
+        }
+    }
+}
