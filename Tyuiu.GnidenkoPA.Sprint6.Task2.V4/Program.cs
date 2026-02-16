3180e7e8708ac2fb6a7dec9d3908c288da98873c
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-16 22:08:10 +0500

    Task2

---
+++ Tyuiu.GnidenkoPA.Sprint6.Task2.V4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Tyuiu.GnidenkoPA.Sprint6.Task2.V4
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
